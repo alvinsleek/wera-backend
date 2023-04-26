588863ddd3b85f3af904e392032c919aa6c605e5
Elizabeth <mwendemuthusi6@gmail.com>
2023-04-26 15:33:04 +0300

    Updated the profile atributes to be rendered in the serializer

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,5 +1,5 @@
 class ProfileSerializer < ActiveModel::Serializer
-  attributes :id, :full_name, :phone_number, :email_address, :created_at 
+  attributes :id, :full_name, :phone_number, :email_address, :created_at, :date_of_birth, :skills, :resume, :biography, :profile_picture
   has_many :tags
   belongs_to :user
   has_many :experiences
